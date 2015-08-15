ecc9588801879053c7726e85a46d65a99d08a711
Ian Fox <ianfox97@gmail.com>
2015-08-15 13:28:00 -0400

    m

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.get('/events', function(request, response) {
   });
 });
 
-app.post('/events', jsonParser, function(request, response) {
-  console.log(request.body);
+app.post('/events', function(request, response) {
+  console.log(request);
   request.event = {"hello": "world"};
   query = 'insert into events (event) values (\'' + JSON.stringify(request.event) + '\') returning id;';
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
