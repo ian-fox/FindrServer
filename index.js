bda5838dd9cda00ce755b31af5d32b3ad07b3a29
Ian Fox <ianfox97@gmail.com>
2015-08-15 12:39:55 -0400

    stuff

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/events', function(request, response) {
 });
 
 app.post('/events', function(request, response) {
-  console.log(request.params);
+  console.log(request);
   request.event = {"hello": "world"};
   query = 'insert into events (event) values (\'' + JSON.stringify(request.event) + '\') returning id;';
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -39,8 +39,7 @@ app.post('/events', function(request, response) {
         console.error(err);
         response.send('Error ' + err);
       } else {
-        console.log(result);
-        response.send("Success!");
+        response.send(result.rows[0].id);
       }
     })
   });
