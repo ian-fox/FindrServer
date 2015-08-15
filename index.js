30876676f0404542baadfcea01329d9ba171db82
Ian Fox <ianfox97@gmail.com>
2015-08-15 12:15:57 -0400

    more fixes, semicolon

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/events', function(request, response) {
   request.event = {"hello": "world"};
   request.id = 2;
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('insert into events values (' + request.id + ' \'' + JSON.stringify(request.event).slice(1, -1) + '\')', function(err, result) {
+    client.query('insert into events values (' + request.id + ' \'' + JSON.stringify(request.event) + '\');', function(err, result) {
       done();
       if (err) {
         console.error(err);
