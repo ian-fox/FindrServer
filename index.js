c9b1ddd8d4e7788ccf27154fb94accae9565c229
Ian Fox <ianfox97@gmail.com>
2015-08-15 12:44:53 -0400

    status line

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/events', function(request, response) {
         console.error(err);
         response.send('Error ' + err);
       } else {
-        response.send('ID: ' + result.rows[0].id);
+        response.send(200, result.rows[0].id);
       }
     })
   });
