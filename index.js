78e7e38605a12e780bf99dfdfca48743a1a07f82
Ian Fox <ianfox97@gmail.com>
2015-08-19 22:04:53 -0400

    no longer just example.com

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var pg = require('pg');
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
